8e116cf474e870efdade5f50dd2693b796c53c34
S D Sriram <sdsriram1@gmail.com>
2023-12-20 21:59:23 +0530

    Some Juicy Code, Finally !

---
+++ tbles_1_to_20.py
@@ -0,0 +1,6 @@
+# Aim : To write a program to generate tables between 1 to 13
+
+for i in range(1,14): # the range is taken fro 1 to 13
+	for j in range(1,11): # the range is taken from 1 to 10
+		print(i,"times",j,"is",i*j)
+	print("=======================================================================")
