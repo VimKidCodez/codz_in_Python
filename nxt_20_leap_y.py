aeffe5a770ea4641706dc812119727020cb1d03f
S D Sriram <sdsriram1@gmail.com>
2023-12-26 15:54:40 +0530

    Just a memory reboot

---
+++ nxt_20_leap_y.py
@@ -0,0 +1,7 @@
+# Write a program to find next twoenty leap year
+
+YEAR = 2024
+
+for i in range(1,21):
+	YEAR = YEAR +4
+	print(YEAR)
---
+++ sq_root_100.py
@@ -0,0 +1,4 @@
+# Write a program to print the square root of numbers from 1 to 100
+
+for i in range(1,100+1):
+	print("The square root of",i,"is",i**2)
